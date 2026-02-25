436b044cc6de4e8dad04731c044478a117dcd5b2
Mathieu Fenniak <mathieu@fenniak.net>
2026-02-24 19:01:18 -0700

    feat: implement fine-grained access tokens in /teams/{id}/repos/{org}/{repo}

--- routers/api/v1/org/team.go
+++ routers/api/v1/org/team.go
@@ -634,9 +634,13 @@ func GetTeamRepo(ctx *context.APIContext) {
 		return
 	}
 
-	permission, err := access_model.GetUserRepoPermission(ctx, repo, ctx.Doer)
+	permission, err := access_model.GetUserRepoPermissionWithReducer(ctx, repo, ctx.Doer, ctx.Reducer)
 	if err != nil {
-		ctx.Error(http.StatusInternalServerError, "GetTeamRepos", err)
+		ctx.Error(http.StatusInternalServerError, "GetUserRepoPermissionWithReducer", err)
+		return
+	}
+	if !permission.HasAccess() {
+		ctx.NotFound()
 		return
 	}
 
